17945404062100952382d592ebdea517899fbfc3
James Heggs <james.heggs@techreturners.com>
2023-02-06 13:26:13 +0000

    fix: Update target kubernetes version

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -2,7 +2,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "17.24.0"
   cluster_name    = local.cluster_name
-  cluster_version = "1.19"
+  cluster_version = "1.24"
   subnets         = module.vpc.private_subnets
 
   tags = {
